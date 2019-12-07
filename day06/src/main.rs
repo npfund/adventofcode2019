f6b195cee8f212a7a2b393bffa138fb9557c118a
Nick Pfundstein <nickpfundstein@gmail.com>
2019-12-07 15:52:29 -0700

    Day 6 Part 2 complete

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,14 +1,28 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
     let file = BufReader::new(File::open("input.txt").unwrap());
     println!("{}", count_orbits(&ingest(file), "COM", 0));
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let system = ingest(file);
+
+    let path_to_you = find_path(&system, "COM", "YOU").unwrap();
+    let path_to_santa = find_path(&system, "COM", "SAN").unwrap();
+
+    let transfers = find_distance(&path_to_you, &path_to_santa);
+
+    println!("{}", transfers);
 }
 
 fn ingest(file: BufReader<File>) -> HashMap<String, Vec<String>> {
@@ -37,6 +51,39 @@ fn count_orbits(system: &HashMap<String, Vec<String>>, node: &str, depth: usize)
         + depth
 }
 
+fn find_path(
+    system: &HashMap<String, Vec<String>>,
+    node: &str,
+    target: &str,
+) -> Option<Vec<String>> {
+    let orbits = system.get(node)?;
+
+    let mut path = Vec::new();
+    path.push(node.to_string());
+    for orbit in orbits {
+        if orbit == target {
+            return Some(path);
+        } else {
+            match find_path(system, orbit, target) {
+                Some(p) => {
+                    path.extend(p.into_iter());
+                    return Some(path);
+                }
+                None => {}
+            }
+        }
+    }
+
+    None
+}
+
+fn find_distance(path1: &Vec<String>, path2: &Vec<String>) -> usize {
+    let path1_nodes: HashSet<String> = path1.clone().into_iter().collect();
+    let path2_nodes: HashSet<String> = path2.clone().into_iter().collect();
+
+    path1_nodes.symmetric_difference(&path2_nodes).count()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -55,5 +102,25 @@ mod test {
 
         let system = ingest(file);
         println!("{}", count_orbits(&system, "COM", 0));
+    }
+
+    #[test]
+    fn test_find_path() {
+        let file = BufReader::new(File::open("test2.txt").unwrap());
+
+        let system = ingest(file);
+        println!("{:?}", find_path(&system, "COM", "YOU"));
+        println!("{:?}", find_path(&system, "COM", "SAN"));
+    }
+
+    #[test]
+    fn test_find_distance() {
+        let file = BufReader::new(File::open("test2.txt").unwrap());
+
+        let system = ingest(file);
+        let path_to_you = find_path(&system, "COM", "YOU").unwrap();
+        let path_to_santa = find_path(&system, "COM", "SAN").unwrap();
+
+        assert_eq!(4, find_distance(&path_to_you, &path_to_santa));
     }
 }
